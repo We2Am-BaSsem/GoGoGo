13ca0dc2394e12b5ae0a814c90b7501672326f3d
Mostafa-wael <mostafa.w.k000@gmail.com>
2021-06-05 15:12:04 +0200

    .

--- Spring_Engine/src/main/java/com/GoGoGo_Search/SearchEngine/document/Link.java
+++ Spring_Engine/src/main/java/com/GoGoGo_Search/SearchEngine/document/Link.java
@@ -1,5 +1,5 @@
 package com.GoGoGo_Search.SearchEngine.document;
-
+// this file includes the definition of the document
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
