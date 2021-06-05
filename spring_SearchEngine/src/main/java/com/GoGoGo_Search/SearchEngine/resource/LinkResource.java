d925263b527b5cc75791070f19dee4cea0f716ff
We'Am <weaam.wewe91@gmail.com>
2021-06-05 16:24:08 +0200

    And Search BackEnd

--- spring_SearchEngine/src/main/java/com/GoGoGo_Search/SearchEngine/resource/LinkResource.java
+++ spring_SearchEngine/src/main/java/com/GoGoGo_Search/SearchEngine/resource/LinkResource.java
@@ -6,62 +6,67 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/rest/links")
 public class LinkResource {
 
     @Autowired
-    private LinkRepo linkRepo;
+    private final LinkRepo linkRepo;
 
     public LinkResource(LinkRepo linkRepo) {
         this.linkRepo = linkRepo;
     }
 
 
-    @RequestMapping(method = RequestMethod.GET ,path = "/findKey")
-    public myJSONdoc getAll(@RequestParam(defaultValue = "") String searchkey, String mode) {
+    @RequestMapping(method = RequestMethod.GET, path = "/findKey")
+    public myJSONdoc getAll(@RequestParam(defaultValue = "") String searchSentence, String mode) {
 
-        //
-        String[] myArr = searchkey.split(" ");
-        System.out.println("\nI was searching for: " +searchkey);
-
-        List<String> searchWords = Arrays.asList(myArr);
+        //  here we split the sentence into words that are used for search process
+        String[] searchWords = searchSentence.split(" ");
 
 
-        //String mode = "Or";//searchkey.substring(searchkey.indexOf('='));
-        System.out.println("I was in mode: " +mode);
-        //
-        ArrayList<String> keys = new ArrayList<>();
-        ArrayList<Document> URLS = new ArrayList<>();
-        ArrayList<Document> temp = new ArrayList<>();
-        //
-        if(mode.equals("Or"))
-        {
-            for (String word: searchWords) {
-                System.out.println("Cuurent Word: " + word);
-                temp = linkRepo.findByKey(word).get(0).getURLS();
-                System.out.println("URLs of: " + word + temp);
-                URLS.addAll(temp);
+        ArrayList<String> keys = new ArrayList<>();     //this list should store the list of words we are looking for
+        ArrayList<Document> URLS = new ArrayList<>();   //this list should store the list of returned documents from the database,
+                                                        // each document contains the URL, Title, Description
+
+
+        //   according to the mode we perform the proper search query query
+        if (mode.equals("Or")) {
+            //key1-> url1 url5 url3
+            //key2-> url5 url3 url7
+            //result-> url1 url5 url3 url7
+
+            ArrayList<Document> URLSOrMode = new ArrayList<>();
+
+            for (String word : searchWords) {
+                URLSOrMode = linkRepo.findByKey(word).get(0).getURLS();
+                URLS.addAll(URLSOrMode);
                 keys.add(word);
             }
+        } else if (mode.equals("And")) {
+            //key1-> url1 url5 url3
+            //key2-> url5 url3 url7
+            //result-> url5 url3
+
+            HashMap<Document, Long> URLSAndMode = new HashMap<>();
+
+            for (String word : searchWords) {
+                ArrayList<Document> queryResult = linkRepo.findByKey(word).get(0).getURLS();
+
+                for (int i = 0; i < queryResult.size(); i++) {
+                    if (URLSAndMode.containsKey(queryResult.get(i))) {
+                        URLSAndMode.put(queryResult.get(i), URLSAndMode.get(queryResult.get(i)) + 1);
+                        URLS.add(queryResult.get(i));
+                    } else {
+                        URLSAndMode.put(queryResult.get(i), 1L);
+                    }
+                }
+            }
         }
-        else if(mode.equals("And"))
-        {
 
-        }
-
-
-        System.out.println("comming Nowwwwwwwww");
-        myJSONdoc doc = new myJSONdoc(keys,URLS, URLS.size());
-
-        System.out.println("I was searching for: " + doc.getKey());
-        System.out.println("list size is: "+doc.getSize());
-        System.out.println("URLs: "+doc.getURLS());
+        myJSONdoc doc = new myJSONdoc(keys, URLS, URLS.size());
 
         return doc;
     }
